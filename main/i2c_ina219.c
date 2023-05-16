3c6a317317de59e22345c447f98a4db98ba17638
Noah Reed <Nore5515@gmail.com>
2023-05-16 16:19:56 -0500

    huge name refactor

--- main/i2c_ina219.c
+++ main/i2c_ina219.c
@@ -1,16 +1,14 @@
-/* i2c - Example
+/**
+ * @file i2c_example_main.c
+ * @author noah reed (nore5515@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-05-16
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
 
-   For other examples please check:
-   https://github.com/espressif/esp-idf/tree/master/examples
-
-   See README.md file to get detailed usage of this example.
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include "esp_log.h"
 #include "driver/i2c.h"
